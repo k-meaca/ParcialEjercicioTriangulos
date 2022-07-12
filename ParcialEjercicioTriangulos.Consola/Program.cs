87c358c8a4afc807f1efdc0857502a1d9058f590
k-meaca <keevme@gmail.com>
2022-07-11 22:45:43 -0300

    Se agrega codigo.

---
+++ ParcialEjercicioTriangulos.Consola/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ParcialEjercicioTriangulos.Entidades;
+
+namespace ParcialEjercicioTriangulos.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Triangulo miTriangulo = new Triangulo();
+
+                Console.WriteLine($"Triangulo de lados ladoA: {miTriangulo.LadoA}, ladoB: {miTriangulo.LadoB}, ladoC: {miTriangulo.LadoC}");
+                Console.WriteLine(miTriangulo.ObtenerPerimetro());
+                Console.WriteLine(miTriangulo.ObtenerSuperficie());
+                Console.WriteLine(miTriangulo.ObtenerTipoTriangulo());
+
+                Triangulo otroTriangulo = new Triangulo(5, 4, 3);
+
+                Console.WriteLine($"Triangulo de lados ladoA: {otroTriangulo.LadoA}, ladoB: {otroTriangulo.LadoB}, ladoC: {otroTriangulo.LadoC}");
+                Console.WriteLine(otroTriangulo.ObtenerTipoTriangulo());
+                Console.WriteLine(otroTriangulo.ObtenerPerimetro());
+                Console.WriteLine(otroTriangulo.ObtenerSuperficie());
+
+                //Triangulo trianguloInvalido;
+
+                //trianguloInvalido = new Triangulo(-5, 4, 3);
+
+                //trianguloInvalido = new Triangulo(10, 1, 1);
+
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ParcialEjercicioTriangulos.Entidades/Triangulo.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcialEjercicioTriangulos.Entidades
+{
+    public class Triangulo
+    {
+        //ATRIBUTOS
+        private int ladoA, ladoB, ladoC;
+
+        //PROPIEDADES
+        public int LadoA { get => ladoA; }
+
+        public int LadoB { get => ladoB; }
+
+        public int LadoC { get => ladoC; }
+
+        //CONSTRUCTORES
+
+        /// <summary>
+        /// Construye un triangulo
+        /// </summary>
+        /// <param name="ladoA">LadoA = Hipotenusa</param>
+        /// <param name="ladoB">LadoB = Cateto</param>
+        /// <param name="ladoC">LadoC = Cateto</param>
+        /// <exception cref="ArgumentException"></exception>
+        public Triangulo(int ladoA, int ladoB, int ladoC)
+        {
+            if (!EsTriangulo(ladoA, ladoB, ladoC))
+            {
+                throw new ArgumentException("Los lados ingresados no forman un triangulo");
+            }
+
+            this.ladoA = ladoA;
+            this.ladoB = ladoB;
+            this.ladoC = ladoC;
+        }
+
+        public Triangulo() : this(1, 1, 1) { }
+
+
+        //METODOS
+        private bool EsTriangulo(int ladoA, int ladoB, int ladoC)
+        {
+            bool valido = false;
+
+            if (ladoA > 0 && ladoB < 0 && ladoC < 0)
+              
+                valido = true;
+            else if ((ladoA + ladoA > ladoC) && (ladoA + ladoC > ladoB) && (ladoB + ladoC > ladoA))
+            
+                valido = true;
+            
+
+            return valido;
+        }
+
+        private bool EsRectangulo()
+        {
+            return Math.Pow(ladoA, 2) == (Math.Pow(ladoB, 2) + Math.Pow(ladoC, 2));
+        }
+
+
+        private bool EsObtusangulo()
+        {
+            return Math.Pow(ladoA, 2) > (Math.Pow(ladoB, 2) + Math.Pow(ladoC, 2));
+        }
+
+        private double Semiperimetro()
+        {
+            return (ladoA + ladoB + ladoC) / 2.0f;
+        }
+
+        //PUBLICOS
+
+        public string ObtenerTipoTriangulo()
+        {
+            string triangulo;
+
+            if (EsRectangulo())
+                triangulo = "Rectangulo";
+            else if (EsObtusangulo())
+                triangulo = "Obtusangulo";
+            else
+                triangulo = "Acutangulo";
+
+            return triangulo;
+        }
+
+        public int ObtenerPerimetro()
+        {
+            return ladoA + ladoB + ladoC;
+        }
+
+        public double ObtenerSuperficie()
+        {
+            return Math.Sqrt(Semiperimetro() * (Semiperimetro() - ladoA) * (Semiperimetro() - ladoB) * (Semiperimetro() - ladoC));
+        }
+    }
+}
